e41b6e6be99b3b38085310fe92c0eba0d0093e65
Iuri Severo <nobreiuri@gmail.com>
2021-04-27 01:03:51 -0300

    'fix' api build problem

--- api/django-entrypoint.sh
+++ api/django-entrypoint.sh
@@ -1,5 +1,7 @@
 echo "Waiting for postgres..."
 
+sleep 10 # until I found a fix for it
+
 wget -qO- https://raw.githubusercontent.com/eficode/wait-for/v2.1.1/wait-for | sh -s -- db:5432 -- echo Success
 
 python3 manage.py migrate
