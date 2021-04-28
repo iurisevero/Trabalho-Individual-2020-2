c48e436e2d1856943198d1e11d1eaedd9579ff07
Iuri Severo <nobreiuri@gmail.com>
2021-04-27 23:59:20 -0300

    Testing remove initdb sh

--- api/postgresql/Postgresql.Dockerfile
+++ api/postgresql/Postgresql.Dockerfile
@@ -1,2 +1,2 @@
 FROM postgres:13.2-alpine
-COPY init_db.sh /docker-entrypoint-initdb.d/
+# COPY init_db.sh /docker-entrypoint-initdb.d/
--- api/postgresql/init_db.sh
+++ api/postgresql/init_db.sh
@@ -1,6 +1,5 @@
+echo "Running init_db.sh"
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    DROP DATABASE IF EXISTS myproject;
-    DROP USER IF EXISTS myuser;
     CREATE DATABASE  myproject;
     CREATE  USER myuser WITH PASSWORD 'password';
     GRANT ALL PRIVILEGES ON DATABASE myproject TO myuser;
