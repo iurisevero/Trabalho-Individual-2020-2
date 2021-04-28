f2cc1e1aa2fdc2c529ee9f07ae684dbf1d6ae493
Iuri Severo <nobreiuri@gmail.com>
2021-04-28 00:37:05 -0300

    Update init_db

--- api/postgresql/init_db.sh
+++ api/postgresql/init_db.sh
@@ -1,6 +1,5 @@
 echo "Running init_db.sh"
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE  myproject;
     CREATE  USER myuser WITH PASSWORD 'password';
     GRANT ALL PRIVILEGES ON DATABASE myproject TO myuser;
     ALTER USER myuser CREATEDB;
