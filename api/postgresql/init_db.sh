67e9c775ee3b97d94a6d0f8fe610b59ce83107e4
Iuri Severo <nobreiuri@gmail.com>
2021-04-29 10:59:38 -0300

    Fix db inicialization

--- api/postgresql/init_db.sh
+++ api/postgresql/init_db.sh
@@ -1,6 +1,7 @@
 echo "Running init_db.sh"
+# o initdb do postgresql cria o banco e define o usuário e a senha
+# não é necessário repetir esse processo aqui
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE  USER myuser WITH PASSWORD 'password';
     GRANT ALL PRIVILEGES ON DATABASE myproject TO myuser;
     ALTER USER myuser CREATEDB;
 EOSQL
